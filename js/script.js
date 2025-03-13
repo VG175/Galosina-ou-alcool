34b672ebcf616f449cc38098958311ec3cdb78a7
Vitor <vg3396355@gmail.com>
2025-03-12 21:27:10 -0300

    Atualização-4
    parte 1 do css concluída, começando a parte do js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// pegar o valor da gasolina e do álcool
+// dividir o valor do álcool pelo da gasolina 
+// se o valor for inferior a 0.7 ou 70%
+// é mais vantajoso abastecer com alcool
+// Se não, gasolina
+
+
+function gerar (){
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value)
+}
